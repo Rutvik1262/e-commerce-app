f781ace770387d01fd07d4a01923ea699f61f1b6
Rutvik <rutvik.avrut@gmail.com>
2024-01-12 09:57:17 -0500

    Initial checkin

---
+++ src/app/core/Model/object-model.ts
@@ -0,0 +1,44 @@
+export class User{
+    name!: string;
+    password!: string;
+    uploadphoto!: string;
+    role!: string;
+    mobNumber!:string;
+    address!:Address;
+    gender!: string;
+    language!: string;
+    email!: string;
+    dob!: string;
+    agreetc!: boolean;
+    age!: number;
+    aboutYou!: string;
+}
+
+export class Address{
+    id!: number;
+    addLine1!: string;
+    addLine2!: string;
+    city!: string;
+    state!: string;
+    Zipcode!:number;
+}
+
+export class Product{
+    id!:number;
+    name!: string;
+    uploadphoto!: string;
+    uploadDesc!: string;
+    mrp!: number;
+    dp!: number;
+    status!: boolean;
+}
+
+export class Order{
+    id!:number;
+    nameId!: number;
+    sellerId!: number;
+    product!: Product;
+    deliveryAddress!: Address;
+    contact!: number;
+    dateTime!: boolean;
+}
---
+++ src/app/shared/services/login-signup.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';        
+import { ApiService } from '../../core/service/api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginSignupService {
+
+  public login_url = "http://localhost:3000";
+  public reg_url = "http://localhost:3000";
+
+  constructor(private http:HttpClient, private apiService:ApiService)  { }
+authlogin(user_name:any, password:any):Observable<any>{
+  return this.apiService.get(this.login_url+'/user?email='+user_name+'&password='+password);
+}
+
+authRegister(user_dto:any, password:any):Observable<any>{
+  return this.apiService.post(this.reg_url+'/user', user_dto)
+}
+
+adminLogin(user_name:any, password:any):Observable<any>{
+  return this.apiService.get(this.login_url+'/user?email='+user_name+'&password='+password+'&role=admin');
+}
+}
